ac2afe4c8a9de93f6e3b038c37c28ceb5d762b74
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-07-28 10:44:41 -0600

    Update propid.js

--- parser/propid.js
+++ parser/propid.js
@@ -27,26 +27,30 @@ fs.readFile(filePath, 'utf8', (err, jsonString) => {
     // Get proposal_ids from command-line argument
     const proposalIds = options.proposal_id;
 
-    // Filter proposals based on command-line argument 'proposalIds'
-    const filteredProposals = data.filter(proposal => {
-      return proposalIds.includes(proposal.proposal_id);
-    });
-
     // Process each filtered proposal
-    filteredProposals.forEach(proposal => {
+    proposalIds.forEach(proposalId => {
+      // Find the proposal specified by proposal_id
+      const proposal = data.find(proposal => proposal.proposal_id === proposalId);
+
+      // Check if the proposal with the given 'proposal_id' exists
+      if (!proposal) {
+        console.log(`No proposal found with proposal_id: ${proposalId}`);
+        return;
+      }
+
       // Extract the description from the filtered proposal
       const description = proposal.content.description;
 
       // Extract the number after '/discussion/' in the description URL
-      const urlPattern = /\/discussion\/(\d+)/;
+      const urlPattern = /https:\/\/commonwealth\.im\/osmosis\/discussion\/(\d+)/;
       const match = description.match(urlPattern);
 
       // Check if the desired URL pattern is found
       if (match) {
         const discussionNumber = parseInt(match[1]);
-        console.log(`Proposal ID: ${proposal.proposal_id}, Discussion Number: ${discussionNumber}`);
+        console.log(`Proposal ID: ${proposalId}, Discussion Number: ${discussionNumber}`);
       } else {
-        console.log(`Proposal ID: ${proposal.proposal_id}, No discussion URL with domain "commonwealth.im" found in the description.`);
+        console.log(`Proposal ID: ${proposalId}, No discussion URL with domain "commonwealth.im" found in the description.`);
       }
     });
   } catch (error) {
